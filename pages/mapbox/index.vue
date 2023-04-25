f5baa303fa93276e4ab74914f8b9c1e1020a2314
Tran Tuan <tuaan.traan.97ltv@gmail.com>
2023-04-25 22:12:20 +0700

    remove file

--- pages/mapbox/index.vue
+++ pages/mapbox/index.vue
@@ -19,6 +19,14 @@ import MapboxDraw from "@mapbox/mapbox-gl-draw";
 import CircleMode from "../../lib/modes/CircleMode";
 import drawStyles from "../../lib/modes/styles";
 import rewind from "@mapbox/geojson-rewind";
+import turfDistance from "@turf/distance";
+import turfArea from "@turf/area";
+import turfAlong from "@turf/along";
+import turfCentroid from "@turf/centroid";
+import * as turfhelp from "@turf/helpers";
+import * as turfMeta from "@turf/invariant";
+import numeral from "numeral";
+
 import { geojsonToLayer } from "../../lib/ui/util";
 
 var dataFeatures = {
@@ -53,12 +61,26 @@ export default {
 			map.addControl(this.drawMap);
 			// listen draw.create
 			map.on("draw.create", this.createLayer);
+			map.on("draw.render", this.onHandleDrawRender);
 			map.on("idle", this.onHandleIdle);
+			this.$SLMap.registerDataSource("_measurements", dataFeatures);
+			this.$SLMap.registerLayer("_measurements", "_measurements", "symbol", {
+				paint: {
+					"text-color": "hsl(234, 100%, 32%)",
+					"text-halo-color": "hsl(0, 0%, 100%)",
+					"text-halo-width": 2,
+				},
+				layout: {
+					"text-field": "{label}",
+					"text-size": 16,
+				},
+			});
 		},
 
 		initMapboxglDraw() {
 			this.drawMap = new MapboxDraw({
 				displayControlsDefault: true,
+				userProperties: true,
 				modes: {
 					...MapboxDraw.modes,
 					draw_circle: CircleMode,
@@ -92,14 +114,15 @@ export default {
 			if (!map) return;
 
 			console.debug("map current data scr", map, this.drawMap);
-			let currentData = this.drawMap.getAll();
-			console.debug("abc", currentData, currentData.features, features);
-			dataFeatures.features = [...currentData.features, ...features];
+			// let currentData = this.drawMap.getAll();
+			// console.debug("abc", currentData, currentData.features, features);
+			const currentFeatures = dataFeatures.features;
+			dataFeatures.features = [...currentFeatures, ...features];
 			dataFeatures = rewind(dataFeatures);
 			console.debug("abc111", dataFeatures);
 
 			// console.debug("getSrc", map.getSource("map-data"));
-			map.getSource("map-data").setData(dataFeatures);
+			// map.getSource("map-data").setData(dataFeatures);
 
 			this.$SLMap.registerDataSource("map-data", dataFeatures);
 			this.$SLMap.registerLayer(
@@ -120,10 +143,79 @@ export default {
 			let map = e.target;
 			if (!map) return;
 			console.debug("map idle");
-			map.addSource("map-data", {
-				type: "geojson",
-				data: dataFeatures,
-			});
+			// map.addSource("map-data", {
+			// 	type: "geojson",
+			// 	data: dataFeatures,
+			// });
+		},
+
+		onHandleDrawRender(e) {
+			// console.debug("onHandleDrawRender");
+			// let map = e.target;
+			// if (!map) return;
+			// var labelFeatures = [];
+			// var all = this.drawMap.getAll();
+			// if (all && all.features) {
+			// 	console.debug("features", all, all.features);
+			// 	all.features.forEach(function (feature) {
+			// 		console.debug("feature", feature);
+			// 		switch (turfMeta.getType(feature)) {
+			// 			case "Point":
+			// 				// label Points
+			// 				if (feature.geometry.coordinates.length > 1) {
+			// 					labelFeatures.push(
+			// 						turfhelp.point(feature.geometry.coordinates, {
+			// 							type: "point",
+			// 							label:
+			// 								feature.geometry.coordinates[1].toFixed(6) +
+			// 								", " +
+			// 								feature.geometry.coordinates[0].toFixed(6),
+			// 						})
+			// 					);
+			// 				}
+			// 				break;
+			// 			case "LineString":
+			// 				// label Lines
+			// 				if (feature.geometry.coordinates.length > 1) {
+			// 					var length = turfDistance(feature.geometry.coordinates);
+			// 					console.debug("abc", length);
+			// 					var label = numeral(length).format("0,0.0a") + "m";
+			// 					var midpoint = turfAlong(
+			// 						feature.geometry.coordinates,
+			// 						length / 2,
+			// 						{ units: "kilometers" }
+			// 					);
+			// 					labelFeatures.push(
+			// 						turfhelp.point(midpoint, {
+			// 							type: "line",
+			// 							label: label,
+			// 						})
+			// 					);
+			// 				}
+			// 				break;
+			// 			case "Polygon":
+			// 				// label Polygons
+			// 				if (
+			// 					feature.geometry.coordinates.length > 0 &&
+			// 					feature.geometry.coordinates[0].length > 3
+			// 				) {
+			// 					var area = turfArea(feature.geometry.coordinates);
+			// 					var label = numeral(area).format("0,0.0a") + "m²";
+			// 					labelFeatures.push(
+			// 						turfCentroid(feature, {
+			// 							type: "area",
+			// 							label: label,
+			// 						})
+			// 					);
+			// 				}
+			// 				break;
+			// 		}
+			// 	});
+			// }
+			// map.getSource("_measurements").setData({
+			// 	type: "FeatureCollection",
+			// 	features: labelFeatures,
+			// });
 		},
 
 		handleVeVongTron() {
